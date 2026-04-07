01602a00b505766cb92d2c133dfbf8949bef1ab9
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-04-07 18:22:24 +0530

    Added Workshop model with foreign key relationships

--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
+from core.models import City
 
-# Create your models here.
+class Workshop(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    seat_limit = models.IntegerField()
+    seats_available = models.IntegerField()
+
+    def __str__(self):
+        return self.title
