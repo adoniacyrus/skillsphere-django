4aa72f234bbe8233df3f6b8a3d039db120de643d
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-04-07 17:28:54 +0530

    Implemented custom user model with role-based system

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('organizer', 'Organizer'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+    def __str__(self):
+        return self.username
