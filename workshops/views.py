aedc3a4559621a0eda6abe6b83176c06dbaf3fea
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-04-08 01:11:25 +0530

    feat: add Workshop list and create API view

--- workshops/views.py
+++ workshops/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Workshop
+from .serializers import WorkshopSerializer
 
-# Create your views here.
+
+class WorkshopListCreateView(generics.ListCreateAPIView):
+    queryset = Workshop.objects.all()
+    serializer_class = WorkshopSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(organizer=self.request.user)
