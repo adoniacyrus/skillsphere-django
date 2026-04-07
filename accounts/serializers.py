7091377eae8f1d9fc921bec4aad04ec456c25962
Adonia Cyrus <adoniacyrus60@gmail.com>
2026-04-07 23:21:42 +0530

    fix: correct API routing for accounts endpoints

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'password','role']
+
+        def create(self, validated_data):
+            password = validated_data.pop('password')
+            user = User(**validated_data)
+            user.set_password(password)  # 🔐 hashes password
+            user.save()
+            return user
